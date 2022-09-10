f91c36e78d3ace3db979b95ee14e9da328626b80
lusatiro <ljsatiro@gmail.com>
2022-09-10 13:22:39 -0300

    install cdk

---
+++ test/learning-aws-vpc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LearningAwsVpc from '../lib/learning-aws-vpc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/learning-aws-vpc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LearningAwsVpc.LearningAwsVpcStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
