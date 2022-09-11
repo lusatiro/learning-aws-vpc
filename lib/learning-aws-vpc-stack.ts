df9ea9e06b3b3a96d51b4a55f4f46fe49cb8a29e
lusatiro <ljsatiro@gmail.com>
2022-09-11 19:31:10 -0300

    add ec2 instances

--- lib/learning-aws-vpc-stack.ts
+++ lib/learning-aws-vpc-stack.ts
@@ -12,26 +12,46 @@ export class LearningAwsVpcStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, "my-vpc", {
+    const vpc = new ec2.Vpc(this, "learning-aws-vpc", {
       vpcName: "my-vpc",
       cidr: "10.0.0.0/26",
       maxAzs: 2,
       subnetConfiguration: [
         {
-          name: "my-private-subnet",
+          name: "private-subnet",
           subnetType: SubnetType.PRIVATE_WITH_NAT,
           cidrMask: 28,
         },
         {
-          name: "my-public-subnet",
+          name: "public-subnet",
           subnetType: SubnetType.PUBLIC,
           cidrMask: 28,
         },
       ],
     });
 
-    new ec2.Instance(this, "my-instance", {
-      instanceName: "my-instance",
+    const securityGroup = new ec2.SecurityGroup(this, "sg", {
+      vpc,
+      securityGroupName: "learning-aws-vpc-sg",
+      allowAllOutbound: true,
+    });
+
+    new ec2.Instance(this, "private-server", {
+      instanceName: "private-server",
+      vpc,
+      vpcSubnets: {
+        subnetType: SubnetType.PRIVATE_WITH_NAT,
+      },
+      instanceType: InstanceType.of(
+        InstanceClass.BURSTABLE2,
+        InstanceSize.NANO
+      ),
+      machineImage: new ec2.AmazonLinuxImage(),
+      securityGroup,
+    });
+
+    new ec2.Instance(this, "public-server", {
+      instanceName: "public-server",
       vpc,
       vpcSubnets: {
         subnetType: SubnetType.PUBLIC,
@@ -41,6 +61,7 @@ export class LearningAwsVpcStack extends cdk.Stack {
         InstanceSize.NANO
       ),
       machineImage: new ec2.AmazonLinuxImage(),
+      securityGroup,
     });
   }
 }
